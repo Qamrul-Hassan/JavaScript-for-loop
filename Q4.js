9bbb6f882ed36d590e4cbebc3bc70ab5814f5c70
Qamrul-Hassan <sajal700@gmail.com>
2024-10-06 23:44:02 +0600

    massage

---
+++ Q4.js
@@ -0,0 +1,7 @@
+/* # 4. print the Multiplication table of 7
+Write a for loop to print the muliplification table of 7. */
+
+let number = 7;
+for (let i= 1; i<= 10; i++) {
+    console.log ("7 * " + i + " = " + (number * i));
+}
