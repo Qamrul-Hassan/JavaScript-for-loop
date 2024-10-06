b9ebf8337e6cb992d8406879c440af11d746702f
Qamrul-Hassan <sajal700@gmail.com>
2024-10-06 14:47:17 +0600

    massage

---
+++ Q1.js
@@ -0,0 +1,8 @@
+/* 1. Sum of numbers from 1 to 100
+Write a for loop to calculate the sum of all numbers from 1 to 100 */
+
+let sum = 0;
+for (let i = 1; i<= 100; i++) {
+    sum += i;
+}
+console.log (" The sum of numbers 1 to 100 is: " + sum);
